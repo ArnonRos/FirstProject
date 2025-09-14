f07443cf82cb3f4815751b87f9ea69059ee6a3eb
arnon <arnon@DESKTOP-R55ETQ8>
2025-09-14 16:23:48 +0300

    Add project files.

---
+++ FirstProject/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public int Age
+        {
+            get { return age; }
+            set { if (value > 0) age = value; }
+
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+
+    }
+}
---
+++ FirstProject/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter mone: ");
+            int mone = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter mechane");
+            int mechane = int.Parse(Console.ReadLine());
+            DecimalFrac dce = new DecimalFrac(mone, mechane);
+            int ans = dce.Calc();
+            int sherit = dce.div();
+            Console.WriteLine("ans: " + ans);
+            Console.WriteLine("sherit: " + sherit);
+
+
+
+        }
+    }
+}
